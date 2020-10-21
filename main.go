9a090a8543936b5253eeef6af7db6e6149993565
Mounir Missaoui <mounir@fivepoints.fr>
2020-10-21 23:35:05 +0100

    first REST api route

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	gin "github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET(
+		"/",
+		func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"data": "Hello World"})
+
+		},
+	)
+	r.Run()
+}
